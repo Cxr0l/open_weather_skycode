68860c7862cd4957da2fe84b197bd97cf1588b2c
Cxr0l <vanerubio2020@gmail.com>
2023-06-10 14:59:59 -0400

    acomodando el API

--- src/index.js
+++ src/index.js
@@ -13,32 +13,12 @@ const coordLong = document.getElementById("longitude");
 const temp = document.getElementById("temp");
 const icon = document.getElementById("icon");
 
-
-
-
 const currenWeather = async (latitude = london.lat, longitude = london.lon) => {
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${keyWeather}`;
     const response = await fetch(url);
     const data = await response.json();
     return data;
 }
-
-
-currenWeather()
-.then(response => {
-    nameCity.innerText = response.name;
-    nameCountry.innerText = response.sys.country;
-    console.log(nameCity);
-    description.innerText = response.weather[0].description;
-    coordLat.innerText = response.coord.lat;
-    coordLong.innerText = response.coord.lon;
-})
-
-
-
-
-
-
 
 const getCoordinateCity = async (nameCity = "Caracas") => {
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${nameCity}&appid=${keyWeather}`; 
@@ -48,63 +28,48 @@ const getCoordinateCity = async (nameCity = "Caracas") => {
     return data;
 };
 
-
-const keyWeather2 = "e293c8239b6b4d53058978c666c2b52a";
-
-const forecast = async (latitude = london.lat, longitude = london.lon) => {
-    const url = `https://api.openweathermap.org/data/2.5/forecast/daily?lat=${latitude}&lon=${longitude}&cnt=7&appid=${keyWeather2}`;
-    const response = await fetch(url);
-    const data = await response.json();
-    return data;
-};
-
-forecast()
-.then(response => {
-    console.log(response);
-})
-.catch(error => console.log(error));
-
-
-
-
-
-
 const convertKelvinToCelsius = (tempKelvin) => {
     return Math.round(tempKelvin - 273.15) + " °C";
 };
 
+const form = document.getElementById("form");
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const cityInput = document.getElementById("city-input").value;
+    getCoordinateCity(cityInput)
+    .then(response => {
+        currenWeather(response.coord.lat, response.coord.lon);
+        nameCity.innerText = response.name;
+        nameCountry.innerText = response.sys.country;
+        description.innerText = response.weather[0].description;
+        coordLat.innerText = response.coord.lat;
+        coordLong.innerText = response.coord.lon;
 
+        temp.innerText = convertKelvinToCelsius(response.main.temp);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-getCoordinateCity("Caracas")
-.then(response => {
-    currenWeather(response.coord.lat, response.coord.lon);
-    nameCity.innerText = response.name;
-    nameCountry.innerText = response.sys.country;
-    description.innerText = response.weather[0].description;
-    coordLat.innerText = response.coord.lat;
-    coordLong.innerText = response.coord.lon;
-
-    temp.innerText = convertKelvinToCelsius(response.main.temp);
-
-    let iconWeather = response.weather[0].icon;
-    icon.setAttribute("src", `https://openweathermap.org/img/wn/${iconWeather}@2x.png`);
-})
+        let iconWeather = response.weather[0].icon;
+        icon.setAttribute("src", `https://openweathermap.org/img/wn/${iconWeather}@2x.png`);
+    })
     .catch(error => console.log(error));
+});
 
 
 
-//HACER UN CONVERTIDOR PARA GRADOS, redondearlo hacia arriba
+
+
+
+
+// const keyWeather2 = "e293c8239b6b4d53058978c666c2b52a";
+
+// const forecast = async (latitude = london.lat, longitude = london.lon) => {
+//     const url = `https://api.openweathermap.org/data/2.5/forecast/daily?lat=${latitude}&lon=${longitude}&cnt=7&appid=${keyWeather2}`;
+//     const response = await fetch(url);
+//     const data = await response.json();
+//     return data;
+// };
+
+// forecast()
+// .then(response => {
+//     console.log(response);
+// })
+// .catch(error => console.log(error));
