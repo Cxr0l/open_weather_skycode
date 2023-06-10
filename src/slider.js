7060e5dba74d85f551727a98cbcca512b7783908
Cxr0l <vanerubio2020@gmail.com>
2023-06-10 14:50:56 -0400

    acomodando el slider

--- src/slider.js
+++ src/slider.js
@@ -1,31 +1,100 @@
-const slider = document.querySelector("#slider");
+// const slider = document.querySelector("#slider");
 
-let sliderSection = document.querySelectorAll(".slider-section");
-let sliderSectionLast = sliderSection[sliderSection.length -1];
+// let sliderSection = document.querySelectorAll(".slider-section1");
+// if (sliderSection.length > 0) {
+//   let sliderSectionLast = sliderSection[sliderSection.length -1];
+//   slider.insertAdjacentElement("afterbegin", sliderSectionLast);
+// }
 
-const btnLeft = document.querySelector("#buttonLeft");
-const btnRight = document.querySelector("#buttonRight");
+// const btnLeft = document.querySelector("#buttonLeft");
+// const btnRight = document.querySelector("#buttonRight");
 
-slider.insertAdjacentElement("afterbegin", sliderSectionLast);
+// function next() {
+//   let sliderSectionFirst = document.querySelectorAll(".slider-section")[0];
+//   if (sliderSectionFirst) {
+//     slider.style.marginLeft = "-200%";
+//     slider.style.transition = "all 0.5s";
+//     setTimeout(function() {
+//       slider.style.transition = "none";
+//       slider.insertAdjacentElement("beforeend", sliderSectionFirst);
+//       slider.style.marginLeft = "-100%";
+//     }, 500);
+//   }
+// }
 
-function next() {
-    let sliderSectionFirst = document.querySelectorAll(".slider-section")[0];
-    slider.style.marginLeft = "-200%";
-    slider.style.transition = "all 0.5s"; // corrección de tipeo aquí
-    setTimeout(function() {
-        slider.style.transition = "none";
-        slider.insertAdjacentElement("beforeend", sliderSectionFirst);
-        slider.style.marginLeft = "-100%";
-    }, 500);
+// function prev() {
+//   let sliderSection = document.querySelectorAll(".slider-section");
+//   let sliderSectionLast = sliderSection[sliderSection.length -1];
+//   if (sliderSectionLast) {
+//     slider.style.marginLeft = "0";
+//     slider.style.transition = "all 0.5s";
+//     setTimeout(function() {
+//       slider.style.transition = "none";
+//       slider.insertAdjacentElement("afterbegin", sliderSectionLast);
+//       slider.style.marginLeft = "-100%";
+//     }, 500);
+//   }
+// }
+
+// btnRight.addEventListener("click", function() {
+//   next();
+// })
+
+// btnLeft.addEventListener("click", function() {
+//   prev();
+// })
+
+
+// Primero, obtenemos los elementos que necesitamos
+const slider = document.getElementById("slider");
+const leftButton = document.getElementById("buttonLeft");
+const rightButton = document.getElementById("buttonRight");
+
+// Establecemos un contador para llevar la cuenta del contenedor actual
+let currentSlide = 0;
+
+// Función que se encarga de mostrar el contenedor actual
+function showSlide() {
+  // Obtenemos todos los contenedores del slider
+  const slides = slider.getElementsByClassName("slider-section1");
+
+  // Movemos los contenedores horizantalmente
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.transform = `translateX(-${currentSlide * 100}%)`;
+  }
 }
 
-function prev() {
-    let sliderSectionLast = sliderSection[sliderSection.length -1]; // utiliza la variable global sliderSection
-    slider.style.marginLeft = "0";
-    slider.style.transition = "all 0.5s"; // corrección de tipeo aquí
-    setTimeout(function() {
-        slider.style.transition = "none";
-        slider.insertAdjacentElement("afterbegin", sliderSectionLast);
-        slider.style.marginLeft = "-100%";
-    }, 500);
+// Función que se ejecuta al hacer clic en el botón de la izquierda
+function slideLeft() {
+  // Restamos 1 al contador
+  currentSlide--;
+
+  // Si llegamos al primer contenedor, volvemos al último
+  if (currentSlide < 0) {
+    currentSlide = slider.getElementsByClassName("slider-section1").length - 1;
+  }
+
+  // Mostramos el contenedor actual
+  showSlide();
 }
+
+// Función que se ejecuta al hacer clic en el botón de la derecha
+function slideRight() {
+  // Sumamos 1 al contador
+  currentSlide++;
+
+  // Si llegamos al último contenedor, volvemos al primero
+  if (currentSlide >= slider.getElementsByClassName("slider-section1").length) {
+    currentSlide = 0;
+  }
+
+  // Mostramos el contenedor actual
+  showSlide();
+}
+
+// Asignamos las funciones a los eventos de clic de los botones
+leftButton.addEventListener("click", slideLeft);
+rightButton.addEventListener("click", slideRight);
+
+// Mostramos el primer contenedor al cargar la página
+showSlide();
